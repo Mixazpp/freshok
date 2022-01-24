1b7430b9eea59854873d2b4e16eda1321503966c
Mihail <sapynui@gmail.com>
2022-01-24 12:12:51 +0200

    12dz + footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function () {
-  
+
+});
+
+
+
+var swiper = new Swiper('.slider', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+
+  navigation: {
+    nextEl: '.slider__swiper-button-next',
+    prevEl: '.slider__swiper-button-prev',
+  },
 });
